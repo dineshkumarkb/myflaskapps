4a219eb2390639989ee5b952bb28635eb26e029e
dineshkumarkb <dineshkumarkb@gmail.com>
2020-08-22 07:26:22 +0530

    WTForms changes

--- trialapp/config.py
+++ trialapp/config.py
@@ -1,7 +1,9 @@
+import os
+
 class DevConfig(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:password@127.0.0.1:5432/restaurantable'
-
+    SECRET_KEY = os.urandom(32)
 
 
 class ProdConfig(object):
--- trialapp/main.py
+++ trialapp/main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import Length, DataRequired, Email, email_validator
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -8,8 +11,8 @@ app.config.from_object(DevConfig)
 
 
 @app.route("/", methods=['GET'])
-def index():
-    return f"<h1>This is my trial app</h1>"
+def home():
+    return render_template('home.html')
 
 
 class Restaurant(db.Model):
@@ -22,6 +25,23 @@ class Restaurant(db.Model):
 
     def __repr__(self):
         return f"Restaurant {self.name}, {self.id}"
+
+
+class CommentForm(FlaskForm):
+    author = StringField('author', validators=[DataRequired()])
+    email = StringField('email', validators=[Email()])
+
+
+@app.route("/submit", methods=['GET', 'POST'])
+def get_form():
+    form = CommentForm()
+    print(f" Received form data {form}")
+    if form.validate_on_submit():
+        print(f" Form validated ")
+        print(f" Form data received {form.author.data}, {form.email.data}", 'info')
+        flash(f" Form data received {form.author.data}, {form.email.data}", 'info')
+        return redirect(url_for('get_form'))
+    return render_template('submit.html', form=form)
 
 
 if __name__ == "__main__":
