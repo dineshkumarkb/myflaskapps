4d2006144de203b619bc60b1c79f408f01bae89a
dineshkumarkb <dineshkumarkb@gmail.com>
2020-09-15 13:49:05 +0530

    Added blueprint changes

--- classapp/main.py
+++ classapp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, jsonify
+from flask import Flask, render_template, url_for, redirect, jsonify, Blueprint
 from flask.views import View
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
@@ -8,7 +8,6 @@ from config import DevConfig
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager,create_access_token,jwt_required, get_jwt_identity, get_current_user
 import uuid
-from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 import os, json
@@ -20,6 +19,16 @@ app.config.from_object(DevConfig())
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
+myblog = Blueprint(name='blog', import_name=__name__)
+
+
+
+@myblog.route("/", methods=["GET"])
+def get_employees():
+
+    emp = EmployeeData.query.filter(EmployeeData.name == "dineshkumarkb").first()
+    print(emp)
+    return jsonify({"name": emp.name})
 
 
 def get_uuid():
@@ -45,18 +54,18 @@ class InputProcessor(View):
 
     def dispatch_request(self):
         emp_form = EmployeeForm()
-        #if emp_form.validate_on_submit():
-        emp_id = get_uuid()
-        name = emp_form.name.data
-        password = emp_form.password.data
-        encrypted_password = bcrypt.generate_password_hash(password)
-        email = emp_form.email.data
-        emp_dict = dict(emp_id=get_uuid(), name=name, password=encrypted_password, email=email)
-        add_employees(emp_dict=emp_dict)
-        print(f" Received emp data {emp_id}, {name}, {email}, {encrypted_password}")
-        #return redirect(url_for("inputprocessor"))
-        return jsonify(f" Form validation failed"), 500
-        #return render_template(self.template_name, form=emp_form)
+        if emp_form.validate_on_submit():
+            emp_id = get_uuid()
+            name = emp_form.name.data
+            password = emp_form.password.data
+            encrypted_password = bcrypt.generate_password_hash(password)
+            email = emp_form.email.data
+            emp_dict = dict(emp_id=get_uuid(), name=name, password=encrypted_password, email=email)
+            add_employees(emp_dict=emp_dict)
+            print(f" Received emp data {emp_id}, {name}, {email}, {encrypted_password}")
+            return redirect(url_for("inputprocessor"))
+
+        return render_template(self.template_name, form=emp_form)
 
 
 def add_employees(emp_dict):
@@ -76,6 +85,8 @@ def add_employees(emp_dict):
 class GetEmployees(View):
 
     methods = ["GET"]
+
+    print(f" The url map is {app.url_map} ")
 
     def dispatch_request(self):
         emp = EmployeeData.query.all()
@@ -117,4 +128,6 @@ if __name__ == "__main__":
     app.add_url_rule("/", view_func=InputProcessor.as_view("inputprocessor",
                                                            template_name="landing.html"))
     app.add_url_rule("/getallemps", view_func=GetEmployees.as_view("getemployees"))
+    app.register_blueprint(myblog, url_prefix="/blog")
     app.run(debug=True)
+
